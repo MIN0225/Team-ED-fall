1ad9ba8c0099d603330f06146769381d81982597
MIN0225 <cro77@naver.com>
2023-10-29 20:43:42 +0900

    Fix: 합주실 찜 이미 찜한 상태라면 데이터 삭제

--- BE/hapjusil/src/main/java/com/hapjusil/service/UserFavoriteService.java
+++ BE/hapjusil/src/main/java/com/hapjusil/service/UserFavoriteService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,14 +27,24 @@ public class UserFavoriteService {
     private PracticeRoomRepository practiceRoomRepository;
 
     @Transactional
-    public UserFavorite addUserFavorite(Long userId, Long practiceRoomId) {
+    public UserFavorite addUserFavorite(Long userId, Long practiceRoomId) { // 합줏실 찜
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         PracticeRoom practiceRoom = practiceRoomRepository.findById(practiceRoomId).orElseThrow(() -> new RuntimeException("PracticeRoom not found"));
 
-        UserFavorite userFavorite = new UserFavorite();
-        userFavorite.setUser(user);
-        userFavorite.setPracticeRoom(practiceRoom);
-        return userFavoriteRepository.save(userFavorite);
+        // 해당 사용자와 연습실 조합으로 이미 찜한 데이터가 있는지 확인
+        Optional<UserFavorite> existingFavorite = userFavoriteRepository.findByUserAndPracticeRoom(user, practiceRoom);
+
+        if (existingFavorite.isPresent()) {
+            // 이미 찜한 상태라면 데이터를 삭제
+            userFavoriteRepository.delete(existingFavorite.get());
+            return null; // 또는 적절한 반환 값을 선택
+        } else {
+            // 찜한 데이터가 없다면 새로 추가
+            UserFavorite userFavorite = new UserFavorite();
+            userFavorite.setUser(user);
+            userFavorite.setPracticeRoom(practiceRoom);
+            return userFavoriteRepository.save(userFavorite);
+        }
     }
 
     public List<PracticeRoom> findFavoriteRoomsByUserId(Long userId) {
