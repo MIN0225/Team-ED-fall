8b987064f65d164738e516d2d1c0f17698295dc5
star2t <star2thb@gmail.com>
2023-11-02 19:17:34 +0900

    Feat: 찜 페이지 작성

--- FE/front/src/pages/Heart.js
+++ FE/front/src/pages/Heart.js
@@ -1,13 +1,88 @@
 import React from "react";
+import ListCard from "../components/Card/ListCard";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "../styles/pages/Heart.css";
 
 function Heart() {
+  const cardData = [
+    {
+      title: "합주실1",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실2",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실3",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실4",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실5",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실6",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실7",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+    {
+      title: "합주실8",
+      cost: "가격",
+      locate: "위치",
+      content: "사진 추가 예정",
+    },
+  ];
+
+  const renderCards = () => {
+    return cardData.map((card, index) => (
+      <ListCard
+        key={index}
+        title={card.title}
+        cost={card.cost}
+        locate={card.locate}
+        content={card.content}
+      />
+    ));
+  };
+
   return (
     <div>
       <Header />
-      <p className="init_height">찜 페이지</p>
+      <div className="heart_title">
+        찜한 합주실
+        <span className="icon-container">
+          <FontAwesomeIcon
+            icon={faHeart}
+            className="far fa-heart heart-icon"
+          />
+        </span>
+      </div>
+      <div className="card_pack">{renderCards()}</div>
       <Footer />
     </div>
   );
